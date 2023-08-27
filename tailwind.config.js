1cdc38fbdc5dd29f6732c1758671c2993dd6c36a
Saroj Kumar Sharma <90202990+sarojkumar2003@users.noreply.github.com>
2023-08-27 14:02:26 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish: ["mulish","sans-serif"],
+      },
+      colors: {
+        deepBlue: "#04032d",
+        LightBlue: "#2b84ea", 
+        LightBlue300: "#4b94ed",
+        LightBlue500: "#0b72e7",
+        greenLight: "#61CEA6",
+        grayText: "818597",
+        LightGray: "#e2e2e2",
+        grayblue: "#344a6c",
+        deepBlueHead: "#162f56",
+        gray2: "#525a76",
+        blacklight: "#181c2e"
+      },
+    },
+  },
+  plugins: [],
+}
+
